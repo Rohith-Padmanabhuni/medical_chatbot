17bc861792dd7e0648e0a9ab827b6f6c04357826
rohith-padmanabhuni <padmanabhunirohith777@gmail.com>
2024-06-20 15:35:36 +0530

    prompt improvement

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import os
 from groq import Groq
 from dotenv import load_dotenv
-from utils import inject_css
-
 
 st.set_page_config(page_title="MedPhi-v1", page_icon="☣")
 
@@ -35,11 +33,7 @@ st.markdown('<div class="fixed-title"><h1>Medical ChatBot</h1></div>', unsafe_al
 if "history" not in st.session_state:
     st.session_state.history = []
 
-if "user_input" not in st.session_state:
-    st.session_state.user_input = ""
-
-def handle_submit():
-    user_input = st.session_state.user_input
+def handle_submit(user_input):
     if user_input:
         chat_completion = client.chat.completions.create(
             messages=[
@@ -54,30 +48,22 @@ def handle_submit():
         st.session_state.history.append({"query": user_input[:30], "response": response})
         st.markdown(f'<div class="query-box"><p>{user_input}</p></div>', unsafe_allow_html=True)
         st.markdown(f'<div class="response-box">Response:<br>{response}</div>', unsafe_allow_html=True)
-        st.session_state.user_input = ""
-
 
 # Function to clear chat history
 def clear_history():
     st.session_state.history = []
 
-# Layout for input and button
-col1, col2,col3 = st.columns([19,1,4])
-with col1:
-    user_input = st.text_input("Enter your query:", placeholder="Message", key="user_input", on_change=handle_submit)
-with col3:
-    pass
-with col2:
-    st.button("➤", key="send-btn", on_click=handle_submit)
-
+# Use st.chat_input for user input
+user_input = st.chat_input("Say something:")
+if user_input:
+    handle_submit(user_input)
 
 # History Display
 st.sidebar.title("History")
-if st.sidebar.button("Clear History",key="clear"):
-    st.session_state.history = []
+if st.sidebar.button("Clear History", key="clear"):
+    clear_history()
 
 for i, entry in enumerate(st.session_state.history):
     if st.sidebar.button(f'{i+1}.{entry["query"][:50]}...', key=f'history_{i}'):
         st.markdown(f'<div class="query-box1"><p>{entry["query"]}</p></div>', unsafe_allow_html=True)
         st.markdown(f'<div class="response-box">{entry["response"]}</div>', unsafe_allow_html=True)
-    
